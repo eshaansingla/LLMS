878f0f8ab8e2a2e1b7947a32762359fde6c1f121
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2025-04-07 19:17:02 +0530

    llms -_-

---
+++ lets-go.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+with open("text.txt",'r',encoding="utf-8") as file:
+	data=file.read()
+chars = sorted(list(set(data)))
+print(len(chars))
+chars_to_int = { ch:i for i, ch in enumerate(chars) }
+int_to_chars = { i:ch for i, ch in enumerate(chars) }
+encode= lambda x: ([chars_to_int[c] for c in x])
+decode= lambda x:''.join([int_to_chars[c] for c in x])
+print(decode(encode('LLMS INTRODUCTION')))
+
+
+
