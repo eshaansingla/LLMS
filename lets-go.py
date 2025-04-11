bca2dc658120aff4db09c4fd5f92b36212a8da94
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2025-04-11 23:58:37 +0530

    11:59 commit

--- lets-go.py
+++ lets-go.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 with open("text.txt",'r',encoding="utf-8") as file:
 	data=file.read()
 chars = sorted(list(set(data)))
@@ -12,5 +13,15 @@ decode= lambda x:''.join([int_to_chars[c] for c in x])
 print(encode('LLMS INTRODUCTION'))
 print(decode(encode('LLMS INTRODUCTION')))
 
+encoded_data = torch.tensor(encode(data), dtype=torch.long)
+n = int(0.8 * len(encoded_data))
+train_data = encoded_data[:n]
+val_data = encoded_data[n:]
+block_size = 8
+for i in range(block_size):
+    x = train_data[i:i+block_size]
+    y = train_data[i+1:i+block_size+1]
+    print(f'Context: {x.tolist()} -> Target: {y.tolist()}')
+    print(f'Decoded Context: "{decode(x.tolist())}" -> Target: "{decode(y.tolist())}"')
 
 
